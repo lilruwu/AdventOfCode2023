41198523b0f2f7051d57eb51ae042cc18e427071
lilruwu <rcg4ueducation@gmail.com>
2023-12-09 11:53:10 +0200

    day 5 part 1 completed

--- Day5/day5_part1.py
+++ Day5/day5_part1.py
@@ -1,7 +1,8 @@
 from collections import defaultdict
 
-with open("example.txt") as f:
+with open("input.txt") as f:
     lines = f.readlines()
+    min_location = -1
 
 
     maps = {
@@ -15,10 +16,10 @@ with open("example.txt") as f:
     }
 
     current_dict = None
-
+# All the numbers of the file are in the dictionary
     for line in lines:
             if line is lines[0]:
-                seeds = lines[0].split(":")[1].strip().split()
+                continue
             elif line == "\n":
                 continue
             else:
@@ -41,10 +42,46 @@ with open("example.txt") as f:
                     current_dict = maps["humidity-to-location map"]
                 else:                                
                     for i in range(int(data[2])):
-                         current_dict[int(data[0])+i] = int(data[1])+i
-                    
+                         current_dict[int(data[1])+i] = int(data[0])+i
+    
+    seeds = lines[0].split(":")[1].strip().split()
 
-                    #print(current_dict)
+    for seed in seeds:
+        if int(seed) in maps["seed-to-soil map"]:
+            
+            soil = int(maps["seed-to-soil map"][int(seed)])
+        else:
+            soil = int(seed)
+        
+        if soil in maps["soil-to-fertilizer map"]:
+            fertilizer = int(maps["soil-to-fertilizer map"][soil])
+        else:
+            fertilizer = soil
+        if fertilizer in maps["fertilizer-to-water map"]:
+            
+            water = int(maps["fertilizer-to-water map"][fertilizer])
+        else:
+            water = fertilizer
+        if water in maps["water-to-light map"]:
+            light = int(maps["water-to-light map"][water])
+        else:
+            light = water
+        if light in maps["light-to-temperature map"]:
+            temperature = int(maps["light-to-temperature map"][light])
+        else:
+            temperature = light
+        if temperature in maps["temperature-to-humidity map"]:
+            humidity = int(maps["temperature-to-humidity map"][temperature])
+        else:
+            humidity = temperature
+        if humidity in maps["humidity-to-location map"]:
+            location = int(maps["humidity-to-location map"][humidity])
+        else:
+            location = humidity
+
+        if location < min_location or min_location == -1:
+            min_location = location
+    print("The result is: ", min_location)
                 
 
 
--- README.md
+++ README.md
@@ -12,12 +12,12 @@ This year is my first year doing this challenge and I am doing it for practising
 - [x] [Day 2](https://adventofcode.com/2023/day/2) :star: :star:
 - [ ] [Day 3](https://adventofcode.com/2023/day/3)
 - [x] [Day 4](https://adventofcode.com/2023/day/4) :star: :star:
-- [ ] [Day 5](https://adventofcode.com/2023/day/5)
+- [ ] [Day 5](https://adventofcode.com/2023/day/5) :star:
 - [ ] [Day 6](https://adventofcode.com/2023/day/6)
 - [ ] [Day 7](https://adventofcode.com/2023/day/7)
 
 
-#### Total: **6** :star:
+#### Total: **7** :star:
 
 ## Day 1
 I didn't enconter that much trouble with the **Part 1** of the problem. **Part 2** was a bit more difficult but I could resolve it at the end with some help in ***Reddit***.
